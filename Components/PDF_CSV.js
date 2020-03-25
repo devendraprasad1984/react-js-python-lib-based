967a61442783cf3f59ef61bf36a94f8e0257c293
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-03-25 17:35:56 +0530

    updates

---
+++ Components/PDF_CSV.js
@@ -0,0 +1,55 @@
+(function () {
+    const {gr,ax}=g_dp_app;
+    const PDF_CSV = () => {
+        let [users, setUsers] = gr.useState([]);
+        gr.useEffect(() => {
+           console.log('pdf/csv is loaded');
+        }, []); //[] to execute component render exactly once and set state of object users
+        let getUsers = (callback) => {
+            ax.get("https://jsonplaceholder.typicode.com/users").then(function (res) {
+                // console.log(res.data);
+                callback(res.data);
+            }).catch(function (err) {
+                console.log(err)
+            });
+        }
+        const rows = [
+            ["name1", "city1", "some other info"],
+            ["name2", "city2", "more info"],
+            ["name2", "city2", "more info"],
+            ["name2", "city2", "more info"],
+            ["name2", "city2", "more info"],
+            ["name2", "city2", "more info"],
+            ["name2", "city2", "more info"],
+        ];
+        let generateCSV=()=>{
+            let delim='|'
+            let csvContent = "data:text/csv;charset=utf-8,"
+                + rows.map(e => e.join(delim)).join("\n");
+            var encodedUri = encodeURI(csvContent);
+            // window.open(encodedUri);//option1
+            var link_download = document.createElement("a");
+            link_download.setAttribute("href", encodedUri);
+            link_download.setAttribute("download", "customers.csv");
+            document.body.appendChild(link_download);
+            link_download.click();
+            document.body.removeChild(link_download);
+        }
+        let displayRows=()=>{
+            return (
+                rows.map(x=><div>{x[0]} | {x[1]} | {x[2]}</div>)
+            )
+        }
+        return (
+            <div>
+                <h1>PDF/CSV Exports goes here</h1>
+                <button className="btn bg-warning text-danger" onClick={()=>generateCSV()}>Generate CSV</button>
+                <div>
+                    {displayRows()}
+                </div>
+            </div>
+        )
+    }
+    g_dp_app.extend('PDF_CSV',PDF_CSV);
+
+})();
--- Components/Routing.js
+++ Components/Routing.js
@@ -3,7 +3,7 @@
     const {HashRouter, Switch, Route, Link}=g_dp_app.gdom;
 
     const setRouting = () => {
-        const {Home,Budget,About,Grid,QRApp,NotFound,DisplayBudgetForms}=g_dp_app;
+        const {Home,Budget,About,Grid,QRApp,NotFound,DisplayBudgetForms,PDF_CSV}=g_dp_app;
         return <HashRouter>
             <div className="bg-info sidenav">
                 <Link to="/">Home</Link>
@@ -11,14 +11,16 @@
                 <Link to="/about">About</Link>
                 <Link to="/grid">Grid</Link>
                 <Link to="/qr">QR</Link>
+                <Link to="/pdf_csv">PDF-CSV</Link>
             </div>
             <div className="rightContainer">
                 <Switch>
-                    <Route exact path="/"><Home/></Route>
+                    <Route exact path="/"><Home /></Route>
                     <Route path="/budget"><Budget title={'Budget Input Form'} callback={DisplayBudgetForms}/></Route>
-                    <Route path="/about"><About/></Route>
-                    <Route path="/grid"><Grid/></Route>
-                    <Route path="/qr"><QRApp/></Route>
+                    <Route path="/about"><About /></Route>
+                    <Route path="/grid"><Grid /></Route>
+                    <Route path="/qr"><QRApp /></Route>
+                    <Route path="/pdf_csv"><PDF_CSV /></Route>
                     <Route path="*" component={NotFound}/>
                 </Switch>
             </div>
