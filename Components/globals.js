5b01e31ed710d8007f4d08ea978d4ace0547c608
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-03-25 17:53:49 +0530

    updates

--- Components/globals.js
+++ Components/globals.js
@@ -6,6 +6,7 @@
     g_dp_app.extend('ag' , window.agGrid);
     g_dp_app.extend('loc' , window.location);
     g_dp_app.extend('fetch' , window.fetch);
+    g_dp_app.extend('pdf' , window.jsPDF);
     g_dp_app.extend('forms', {
         names: ['Project Details', 'Capex/Revx', 'Costs', 'Benefits'],
         0: [{rowName: 'Net Interest Income', cols: [0, 0, 0, 0, 0]}, {
